edf478c0bedd4e2892cbad3ff4c0ca1f830ed828
Ilona Shulha <ilona.sh.ilo@gmail.com>
2023-08-08 14:04:54 +0300

    base was added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue_dynamic-list-of-posts/',
+  base: '/list-of-posts-vue/',
   plugins: [
     vue(),
   ],
